7ac84b3d451e320bc1bf3e607f39babc0a1652e1
DanielT4 <daniel_eriksson97@hotmail.com>
2016-10-10 15:38:51 +0200

    About page now prints a list of my relevant experiences.

--- WebAssignmentOct/Controllers/HomeController.cs
+++ WebAssignmentOct/Controllers/HomeController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAssignmentOct.Models;
 
 namespace WebAssignmentOct.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Default
+        private List<Experience> experiences;
+
+        public HomeController()
+        {
+            experiences = new List<Experience>();
+            experiences.Add(new Experience() { education = "Digital Creation", period = "2014-2015" });
+            experiences.Add(new Experience() { education = "Web development", period = "2014-2015" });
+            experiences.Add(new Experience() { education = "Programming 1", period = "2015-2016" });
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -16,7 +26,7 @@ namespace WebAssignmentOct.Controllers
 
         public ActionResult About()
         {
-            return View();
+            return View(experiences);
         }
 
         public ActionResult Contact()
